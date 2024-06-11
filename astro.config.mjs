eb08618818dcf071d6a41f17a26551fa2067e05a
Nitrrine <43351072+Nitrrine@users.noreply.github.com>
2024-06-11 12:37:43 +0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config"
+
+// https://astro.build/config
+export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+  site: "https://stopmalwarecontent.github.io",
+  base: "/",
+})
