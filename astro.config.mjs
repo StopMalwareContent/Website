1471064422ad2cfa3ea3dbc4d94cdc871f210298
Nitrrine <43351072+Nitrrine@users.noreply.github.com>
2024-06-23 11:02:34 +0500

    Enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config"
+import cloudflare from "@astrojs/cloudflare"
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+  output: "server",
+  adapter: cloudflare(),
   site: "https://stopmalwarecontent.lodine.xyz",
 })
