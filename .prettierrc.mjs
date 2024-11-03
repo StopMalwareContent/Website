db307ed5737f081d26e4d0ed396823764e33e6aa
Nitrrine <nitrrine@team.lodine.xyz>
2024-11-04 01:01:29 +0500

    Apply Prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  useTabs: false,
+  semi: false,
+  tabWidth: 2,
+}
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from "astro/config"
+import cloudflare from "@astrojs/cloudflare"
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
   output: "server",
   adapter: cloudflare(),
   site: "https://stopmalwarecontent.lodine.xyz",
-  integrations: [sitemap()]
-});
+  integrations: [sitemap()],
+})
